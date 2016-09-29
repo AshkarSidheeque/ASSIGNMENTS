ef050aac978a1deb58a78c8dcc14eede5825e7f0
ashycodes <ashycodes@gmail.com>
2016-09-29 11:19:18 +0530

    Small changes for variable names

--- python/list.py
+++ python/list.py
@@ -1,48 +1,46 @@
 class ItemList:
 		def __init__(self, list):
-			self.a = list
+			self.NewList = list
 
 		def add(self):
 			item = raw_input("Enter item : ")
-			self.a.append(item)
-			print self.a
+			self.NewList.append(item)
+			print self.NewList
 
 		def delete(self):
-			if len(self.a) == 0:
+			if len(self.NewList) == 0:
 				print "Sorry the list is empty"
 			else:
-				print self.a
+				print self.NewList
 				c = raw_input("which item to delete?")
-				while c in self.a: 
-					self.a.remove(c)
-				print self.a
+				while c in self.NewList: 
+					self.NewList.remove(c)
+				print self.NewList
 
 		def display(self): 
-			if len(self.a) == 0:
+			if len(self.NewList) == 0:
 				print "No items in the list !!"
 			else:
 				print "This is your list"
-				print self.a
-
-
-ans = 1
-a=[]
-myList = ItemList(a)
-while ans:
+				print self.NewList
+choice = 1
+NewList=[]
+myList = ItemList(NewList)
+while choice:
 	print ("""
 	1.Add Item
 	2.View Item
 	3.Delete Item
 	4.Exit
 	""")
-	ans=raw_input("What would you like to do? ") 
-	if ans=="1": 
+	choice=raw_input("What would you like to do? ") 
+	if choice=="1": 
 		myList.add()
-	elif ans=="2":
+	elif choice=="2":
 		myList.display()
-	elif ans=="3":
+	elif choice=="3":
 		myList.delete() 
-	elif ans=="4":
+	elif choice=="4":
 		print("\n Goodbye") 
 		exit()
 	else:
