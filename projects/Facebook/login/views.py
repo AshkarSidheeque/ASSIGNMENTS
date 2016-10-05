c04e058b2b9830ec5ca9b72c2567333c86c6a5cc
ashycodes <ashycodes@gmail.com>
2016-10-05 11:13:53 +0530

    small modifications

--- projects/Facebook/login/views.py
+++ projects/Facebook/login/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.template import Template
 from django.shortcuts import render
